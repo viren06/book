9262eb9c12e4c66d700c4388367edd7300262215
viren <viren000p@gmail.com>
2021-08-18 11:11:00 +0530

    login

--- app/src/main/java/com/example/book1/MainActivity.kt
+++ app/src/main/java/com/example/book1/MainActivity.kt
@@ -2,9 +2,6 @@ package com.example.book1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.TextUtils
-import android.view.View.inflate
-import android.widget.Toast
 import androidx.core.content.res.ColorStateListInflaterCompat.inflate
 import androidx.core.content.res.ComplexColorCompat.inflate
 import androidx.core.graphics.drawable.DrawableCompat.inflate
