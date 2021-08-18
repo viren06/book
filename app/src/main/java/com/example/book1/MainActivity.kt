f3089ea3c75ff96cd393effec6b6de027a5bf710
viren <viren000p@gmail.com>
2021-08-18 10:31:58 +0530

    first

--- app/src/main/java/com/example/book1/MainActivity.kt
+++ app/src/main/java/com/example/book1/MainActivity.kt
@@ -2,13 +2,28 @@ package com.example.book1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.view.View.inflate
+import android.widget.Toast
+import androidx.core.content.res.ColorStateListInflaterCompat.inflate
+import androidx.core.content.res.ComplexColorCompat.inflate
+import androidx.core.graphics.drawable.DrawableCompat.inflate
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+   // private lateinit var binding: ActivityMa
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
-    }
+        //binding = MainActivity.inflate
+
+
+
+
+}
 }
