80f2bf50de451f5897a6e46c88fc1eca1031fb47
mihir9111 <patelmihir9111@gmail.com>
2021-08-21 23:20:39 +0530

    clgdepartment add

---
+++ app/src/main/java/com/example/book1/pdpiasdepartment.kt
@@ -0,0 +1,11 @@
+package com.example.book1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class pdpiasdepartment : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pdpiasdepartment)
+    }
+}
